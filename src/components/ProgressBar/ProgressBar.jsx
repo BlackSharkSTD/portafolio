ebd4dbd3783817cfa4ee06b3ac5f7f262ad67385
MeiwaMexico <89144881+BlackSharkSTD@users.noreply.github.com>
2022-12-07 17:25:04 -0600

    2022-12-07
    Componente de ProgressBar

---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/styles/components.css';
+
+export default function PBTwitter({size}) {
+  return (
+    <>
+        <div className="progress-bar">
+            <div className="pb-dev" width={size} color="" />
+        </div>
+    </>
+  )
+}
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import '../assets/styles/styles.css';
+import '../assets/styles/components.css';
+import PBTwitter from '../components/ProgressBar/ProgressBar';
 
 export default function AboutPage() {
   return (
     <div>
-        <h1>AboutPage</h1>
+        <header>
+          <Navbar />
+        </header>
+        <section>
+          <h1>About Page</h1>
+          <div className="spacing5" />
+          <div className="progress-bar">
+            <div className="pb-twitter" />
+          </div>
+          <div className="spacing15" />
+          <div className="progress-bar">
+            <div className="pb-dev" />
+          </div>
+        </section>
     </div>
   )
 }
