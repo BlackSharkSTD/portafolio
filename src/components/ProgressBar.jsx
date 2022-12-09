6577e909b0d5404c62e4fd3d11c38fc204aa0bcd
MeiwaMexico <89144881+BlackSharkSTD@users.noreply.github.com>
2022-12-09 17:23:46 -0600

    2022-12-09
    Se hicieron cambios a los progress bar y al sistema de procentajes

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../assets/styles/components.css';
 
-export const ProgressBar = ({size, color}) => {
+export const ProgressBar = ({size, color, title}) => {
   return(
-    <div className="progress">
-      <hr className="barr" width={size} color={color} />
+    <div className="progress-bar">
+      <label>{title}</label>
       <p>{size}</p>
+      <div className="progress">
+        <hr className="barr" width={size} color={color} />
+      </div>
     </div>
   );
 };
