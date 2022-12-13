b2e14c463370ae20ae4a85f658d0be268f3913d1
user <89144881+BlackSharkSTD@users.noreply.github.com>
2022-12-12 23:53:13 -0600

    2022-12-13
    Agregar mas iconos y acomodar estructura de datos

--- src/containers/AboutPage/CodeSection.jsx
+++ src/containers/AboutPage/CodeSection.jsx
@@ -1,60 +1,25 @@
 import React from 'react';
-import { programs } from '../../services/xconfig';
+import { programs } from '../../services/xconfig.jsx';
 import { ProgressBar } from '../../components/ProgressBar';
-import { FaJsSquare, FaCss3Alt, FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
+
 import '../../assets/styles/components.css';
 
 export default function CodeSection() {
   return (
     <>
-        <div className="spacing10" />
-        <div className="cart">
-            <div className="divicon">
-                <FaHtml5 className="icon-html" />
-            </div>
-            <ProgressBar title="HTML" size="95%" color="#FF5722" />
-        </div>
-        <div className="spacing10" />
-        <div className="cart">
-            <div className="divicon">
-                <FaCss3Alt className="icon-css" />
-            </div>
-            <ProgressBar title="CSS3" size="85%" color="#2196F3" />
-        </div>
-        <div className="spacing10" />
-        <div className="cart">
-            <div className="divicon">
-                <FaJsSquare className="icon-js" />
-            </div>
-            <ProgressBar title="Javascript" size="80%" color="#F7DE1F" />
-        </div>
-        <div className="spacing10" />
-            <div className="cart">
-            <div className="divicon">
-                <FaNodeJs className="icon-node" />
-            </div>
-            <ProgressBar title="NodeJS" size="50%" color="#82C000" />
-        </div>
-        <div className="spacing10" />
-            <div className="cart">
-            <div className="divicon">
-                <FaReact className="icon-react" />
-            </div>
-            <ProgressBar title="React" size="90%" color="#61DAFB" />
-        </div>
         {programs.map((e) => {
             return(
                 <>
                     <div className="spacing10" />
-                    <div className="cart">
+                    <div className="cart" key={e.id}>
                         <div className="divicon">
-                            
+                            <img src={e.icon} color={e.color} />
                         </div>
                         <ProgressBar title={e.title} size={e.porcent} color={e.color} />
                     </div>
                 </>
             );
-        })};
+        })}
     </>
   )
 }
--- src/services/xconfig.jsx
+++ src/services/xconfig.jsx
@@ -9,9 +9,17 @@ import JavaIcon from '../assets/svg/java.svg';
 import WindowsIcon from '../assets/svg/windows.svg';
 import WindowsSIcon from '../assets/svg/windowsS.svg';
 import DebianIcon from '../assets/svg/debian.svg';
-import GravitDesignerIcon from '../assets/svg/gravitD.svg';
+import FigmaIcon from '../assets/svg/figma.svg';
 import PhotoshopIcon from '../assets/svg/photoshop.svg';
 import FirebaseIcon from '../assets/svg/firebase.svg';
+import VBIcon from '../assets/svg/visualbasic.svg';
+import TSIcon from '../assets/svg/typescript.svg';
+import MYSQLIcon from '../assets/svg/mysql.svg';
+import MongoIcon from '../assets/svg/mongo.svg';
+import MSQLSERVER from '../assets/svg/msqlserver.svg';
+import AccessIcon from '../assets/svg/access.svg';
+import SQLiteIcon from '../assets/svg/sqlite.svg';
+import LinuxIcon from '../assets/svg/linux.svg';
 import MexicoFlag from '../assets/svg/mexico.svg';
 import UsaFlag from '../assets/svg/usa.svg';
 import ItaliaFlag from '../assets/svg/italia.svg';
@@ -42,12 +50,11 @@ export const programs = [
         id: 4,
         title: 'NodeJS',
         icon: NodeJSIcon,
-        porcent: '30%',
+        porcent: '45%',
         color: '#82C000',
     },
     {
         id: 5,
-        name: 'icon-react',
         title: 'React',
         icon: ReactIcon,
         porcent: '90%',
@@ -55,67 +62,138 @@ export const programs = [
     },
     {
         id: 6,
-        name: 'icon-bash',
+        title: 'Typescript',
+        icon: TSIcon,
+        porcent: '70%',
+        color: '#007ACC',
+    },
+    {
+        id: 7,
         title: 'Bash Scripting',
         icon: BashIcon,
         porcent: '99%',
         color: '#F2F2F2',
     },
     {
-        id: 7,
+        id: 8,
         title: 'Batch',
         icon: BatchIcon,
         porcent: '99.5%',
         color: '#B8C9DD',
     },
     {
-        id: 8,
+        id: 9,
         title: 'Java',
         icon: JavaIcon,
         porcent: '36%',
         color: '#F44336',
     },
     {
-        id: 9,
-        title: 'Windows 7/8/10',
+        id: 10,
+        title: 'Visual Basic .NET',
+        icon: VBIcon,
+        porcent: '73%',
+        color: '#376DB7',
+    },
+];
+
+export const systems = [
+    {
+        id: 1,
+        title: 'Windows 7/8/8.1/10',
         icon: WindowsIcon,
         porcent: '88%',
         color: '#2196F3',
     },
     {
-        id: 10,
+        id: 2,
         title: 'Windows Server 2016',
         icon: WindowsSIcon,
         porcent: '76%',
         color: '#00AFF0',
     },
     {
-        id: 11,
+        id: 3,
         title: 'Debian',
         icon: DebianIcon,
         porcent: '80%',
         color: '#E91E63',
     },
     {
-        id: 12,
+        id: 4,
+        title: 'Linux/Linux Lite',
+        icon: LinuxIcon,
+        porcent: '80%',
+        color: '#FFC107',
+    },
+];
+
+export const design = [
+    {
+        id: 1,
         title: 'Gravit Designer',
-        icon: GravitDesignerIcon,
+        icon: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Gravit_Designer_Logo.svg",
         porcent: '90%',
         color: '#D81262',
     },
     {
-        id: 13,
+        id: 2,
         title: 'Adobe Photoshop',
         icon: PhotoshopIcon,
         porcent: '39%',
         color: '#03A9F4',
     },
     {
-        id: 14,
+        id: 3,
+        title: 'Figma',
+        icon: FigmaIcon,
+        porcent: '43%',
+        color: '#A259FF',
+    },
+];
+
+export const database = [
+    {
+        id: 1,
+        title: 'Microsoft SQL SERVER',
+        icon: MSQLSERVER,
+        porcent: '85%',
+        color: '#EE352C',
+    },
+    {
+        id: 1,
         title: 'Firebase',
         icon: FirebaseIcon,
         porcent: '69%',
         color: '#FFA000',
+    },
+    {
+        id: 2,
+        title: 'MySQL',
+        icon: MYSQLIcon,
+        porcent: '44%',
+        color: '#00758F',
+    },
+    {
+        id: 3,
+        title: 'MongoDB',
+        icon: MongoIcon,
+        porcent: '65%',
+        color: '#41A247',
+    },
+    {
+        id: 4,
+        title: 'SQLite',
+        icon: SQLiteIcon,
+        porcent: '70%',
+        color: '#0F80CC',
+    },
+    {
+        id: 5,
+        title: 'Microsoft Access',
+        icon: AccessIcon,
+        porcent: '35%',
+        color: '#B72C3F',
     },
 ];
 
